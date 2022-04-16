9dce6ab1374395de7fc0be9000060f91f01892d7
Amirhossein Mehrabi <a.mehrabi.70@gmail.com>
2022-04-17 00:09:18 +0430

    feat: update image component default props

--- src/core/display/Image.tsx
+++ src/core/display/Image.tsx
@@ -5,7 +5,7 @@ import "./Image.scss";
 type Props = ImageProps & {};
 
 const Image = (props: Props) => {
-  return <AntdImage {...props} />;
+  return <AntdImage {...props} preview={false} width={"100%"} />;
 };
 
 export default Image;
