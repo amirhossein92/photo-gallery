54d4374ace185994833fed9f5e05116139ed9836
Amirhossein Mehrabi <a.mehrabi.70@gmail.com>
2022-04-17 00:17:31 +0430

    feat: add masonry for product gallery view

--- src/components/Product/ProductGallery.tsx
+++ src/components/Product/ProductGallery.tsx
@@ -1,5 +1,6 @@
 import classNames from "classnames";
 import { isEmpty } from "lodash";
+import Masonry from "react-masonry-css";
 
 import { Product } from "types/product";
 import ProductCard from "./ProductCard";
@@ -10,12 +11,25 @@ type Props = React.HTMLAttributes<HTMLDivElement> & {
   items: Product[] | undefined;
 };
 
+const breakpointColumnsObj = {
+  default: 6,
+  1100: 4,
+  700: 2,
+  500: 1,
+};
+
 const ProductGallery = ({ items, className, ...rest }: Props) => {
   return (
     <div className={classNames("product-gallery", className)} {...rest}>
-      {items &&
-        !isEmpty(items) &&
-        items.map((item, index) => <ProductCard key={index} {...item} />)}
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className="product-gallery__grid"
+        columnClassName="product-gallery__grid-column"
+      >
+        {items &&
+          !isEmpty(items) &&
+          items.map((item, index) => <ProductCard key={index} {...item} />)}
+      </Masonry>
     </div>
   );
 };
