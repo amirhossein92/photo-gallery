40ea0c6f649ac5839c9cae1af7b2e3847e14aed1
Amirhossein Mehrabi <a.mehrabi.70@gmail.com>
2022-04-17 23:20:53 +0430

    feat: add intersection observer for product card

--- src/components/Product/ProductCard.tsx
+++ src/components/Product/ProductCard.tsx
@@ -2,6 +2,8 @@ import classNames from "classnames";
 
 import Image from "core/display/Image";
 import TagList from "core/display/TagList";
+import useIntersectionObserver from "hooks/view/useIntersectionObserver";
+import { useRef } from "react";
 
 import { Product } from "types/product";
 
@@ -18,20 +20,35 @@ const ProductCard = ({
   className,
   ...rest
 }: Props) => {
+  const currentRef = useRef<HTMLDivElement>(null);
+  const isVisible = useIntersectionObserver(
+    currentRef as React.MutableRefObject<HTMLElement>
+  );
+
+  console.log({ isVisible });
+
   return (
-    <div className={classNames("product-card", className)} {...rest}>
-      <Image src={imageUrl} className="product-card__img" />
-      <TagList tags={tags} className="product-card__tags" />
-      {name && <h5 className="product-card__name">{name}</h5>}
-      {description && (
-        <p
-          className="product-card__description"
-          dangerouslySetInnerHTML={{
-            __html: description.startsWith("<")
-              ? description
-              : `${description}`,
-          }}
-        />
+    <div
+      className={classNames("product-card", className)}
+      {...rest}
+      ref={currentRef}
+    >
+      {isVisible && (
+        <>
+          <Image src={imageUrl} className="product-card__img" />
+          <TagList tags={tags} className="product-card__tags" />
+          {name && <h5 className="product-card__name">{name}</h5>}
+          {description && (
+            <p
+              className="product-card__description"
+              dangerouslySetInnerHTML={{
+                __html: description.startsWith("<")
+                  ? description
+                  : `${description}`,
+              }}
+            />
+          )}
+        </>
       )}
     </div>
   );
